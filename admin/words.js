3d29be036c11b2db5f6870d8c57f6a5723bda916
swissglider <npm@mailschweiz.com>
2022-07-08 17:57:50 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'sg_webapi adapter settings': {
+        en: 'Adapter settings for sg_webapi',
+        de: 'Adaptereinstellungen für sg_webapi',
+        ru: 'Настройки адаптера для sg_webapi',
+        pt: 'Configurações do adaptador para sg_webapi',
+        nl: 'Adapterinstellingen voor sg_webapi',
+        fr: "Paramètres d'adaptateur pour sg_webapi",
+        it: "Impostazioni dell'adattatore per sg_webapi",
+        es: 'Ajustes del adaptador para sg_webapi',
+        pl: 'Ustawienia adaptera dla sg_webapi',
+        'zh-cn': 'sg_webapi的适配器设置',
+    },
+    option1: {
+        en: 'option1',
+        de: 'Option 1',
+        ru: 'Опция 1',
+        pt: 'Opção 1',
+        nl: 'Optie 1',
+        fr: 'Option 1',
+        it: 'opzione 1',
+        es: 'Opción 1',
+        pl: 'opcja 1',
+        'zh-cn': '选项1',
+    },
+    option2: {
+        en: 'option2',
+        de: 'Option 2',
+        ru: 'вариант 2',
+        pt: 'opção 2',
+        nl: 'Optie 2',
+        fr: 'Option 2',
+        it: 'opzione 2',
+        es: 'opcion 2',
+        pl: 'Opcja 2',
+        'zh-cn': '选项2',
+    },
+};
